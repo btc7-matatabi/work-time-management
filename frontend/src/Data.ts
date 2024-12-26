22bba44c1a98f3a83109ca10a4b68e08f07db34b
natsuki_suzuki <natsuki_suzuki_aa@mail.toyota.co.jp>
2024-12-26 16:03:10 +0900

    作業項目テーブル作成

--- frontend/src/Data.ts
+++ frontend/src/Data.ts
@@ -145,3 +145,16 @@ export const scheduleType = [
   { id: 9, name: 'その他'}
 ]
 
+export const workContents = [
+  { id: 1, group_code: 'LT441', work_content: 'TODO_1 hogehoge', order_number: '0000-0000-0000-0000', total_work_minute: 360 }
+]
+
+export const workHourResult = [
+  { work_contents_id: 1, ymd: '2024/12/2', work_minute: 60 },
+  { work_contents_id: 1, ymd: '2024/12/3', work_minute: 60 },
+  { work_contents_id: 1, ymd: '2024/12/4', work_minute: 90 },
+  { work_contents_id: 1, ymd: '2024/12/5', work_minute: 30 },
+  { work_contents_id: 1, ymd: '2024/12/6', work_minute: 60 },
+  { work_contents_id: 1, ymd: '2024/12/9', work_minute: 30 }
+]
+
---
+++ frontend/src/WorkContentsTable.tsx
@@ -0,0 +1,59 @@
+import {Table, TableBody, TableCell, TableFooter, TableHead, TableHeader, TableRow} from "@/components/ui/table.tsx"
+
+//サンプルデータ
+import {workContents} from "@/Data.ts";
+import {workHourResult} from "@/Data.ts";
+
+//CSS
+const headerCss = "text-center border"
+const contentsCss = "text-center border"
+
+function restWorkHour(id : number, totalTime : number) {
+  return (totalTime * 60) - workHourResult.filter(result => {
+    return result.work_contents_id === id
+  }).reduce((sum, time) => {
+    return sum + time.work_minute;
+  },0)
+}
+
+function totalWorkHour(id : number) {
+  return workHourResult.filter(result => {
+    return result.work_contents_id === id;
+  }).reduce((sum, time) => {
+    return sum + time.work_minute
+  },0)
+}
+
+export function WorkContentsTable() {
+  return(
+    <div className="flex-shrink-0">
+      <Table className="bg-gray-50 text-2xl overflow-hidden">
+        <TableHeader>
+          <TableRow className="h-14">
+            <TableHead className={`${headerCss} w-80`}>作業内容</TableHead>
+            <TableHead className={`${headerCss} w-80`}>管理番号</TableHead>
+            <TableHead className={`${headerCss} w-32`}>全工数</TableHead>
+            <TableHead className={`${headerCss} w-32`}>残工数</TableHead>
+            <TableHead className={`${headerCss} w-32`}>今月合計</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {workContents.map((content => {
+            const viewTime = restWorkHour(content.id, content.total_work_minute)
+            const totalMinute = totalWorkHour(content.id)
+            return (
+              <TableRow>
+                <TableCell className={contentsCss}>{content.work_content}</TableCell>
+                <TableCell className={contentsCss}>{content.order_number}</TableCell>
+                <TableCell className={contentsCss}>{content.total_work_minute}</TableCell>
+                <TableCell className={contentsCss}>{`${Math.floor(viewTime/60)}:${('00' + (viewTime%60)).slice(-2)}`}</TableCell>
+                <TableCell className={contentsCss}>{`${Math.floor(totalMinute/60)}:${('00' + (totalMinute%60)).slice(-2)}`}</TableCell>
+              </TableRow>
+            )
+          }))}
+        </TableBody>
+        <TableFooter></TableFooter>
+      </Table>
+    </div>
+  )
+}
